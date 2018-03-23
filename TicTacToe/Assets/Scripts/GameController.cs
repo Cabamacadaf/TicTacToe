3f5473a3b4b681a6ad6c21315537be99da47a426
Cabamacadaf <maccus9@hotmail.com>
2018-03-23 13:11:02 +0100

    more code

--- TicTacToe/Assets/Scripts/GameController.cs
+++ TicTacToe/Assets/Scripts/GameController.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
     public Text[] buttonList;
+    public GameObject gameOverPanel;
+    public Text gameOverText;
 
     private string playerSide;
+    private int moveCount;
 
     void Awake () {
+        gameOverPanel.SetActive(false);
         SetGameControllerReferenceOnButtons();
         playerSide = "X";
+        moveCount = 0;
     }
 
     void SetGameControllerReferenceOnButtons () {
@@ -24,6 +29,7 @@ public class GameController : MonoBehaviour {
     }
 
     public void EndTurn () {
+        moveCount++;
         if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide) {
             GameOver();
         }
@@ -54,6 +60,9 @@ public class GameController : MonoBehaviour {
         if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide) {
             GameOver();
         }
+        if(moveCount >= 9) {
+            SetGameOverText("It's a draw!");
+        }
         ChangeSides();
     }
 
@@ -66,9 +75,29 @@ public class GameController : MonoBehaviour {
         }
     }
 
+    void SetGameOverText(string value) {
+        gameOverPanel.SetActive(true);
+        gameOverText.text = value;
+    }
+
     void GameOver () {
+        SetBoardInteractable(false);
+        SetGameOverText(playerSide + " Wins!");
+    }
+
+    public void RestartGame () {
         for (int i = 0; i < buttonList.Length; i++) {
-            buttonList[i].GetComponentInParent<Button>().interactable = false;
+            buttonList[i].text = "";
+            SetBoardInteractable(true);
+        }
+        playerSide = "X";
+        moveCount = 0;
+        gameOverPanel.SetActive(false);
+    }
+
+    void SetBoardInteractable (bool toggle) {
+        for (int i = 0; i < buttonList.Length; i++) {
+            buttonList[i].GetComponentInParent<Button>().interactable = toggle;
         }
     }
 }
