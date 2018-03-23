5c8cb8a9594104ba4606e30daabda6dac9b51c64
Cabamacadaf <maccus9@hotmail.com>
2018-03-23 12:50:06 +0100

    Mera saker

--- TicTacToe/Assets/Scripts/GridSpace.cs
+++ TicTacToe/Assets/Scripts/GridSpace.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 public class GridSpace : MonoBehaviour {
 	public Button button;
 	public Text buttonText;
-	public string playerSide;
 
-	public void SetSpace () {
-		buttonText.text = playerSide;
+    private GameController gameController;
+    public void SetGameControllerReference (GameController gameController) {
+        this.gameController = gameController;
+    }
+
+    public void SetSpace () {
+		buttonText.text = gameController.GetPlayerSide();
 		button.interactable = false;
+        gameController.EndTurn();
 	}
 }
